f8331d8d48124157a2b754e08b5a5f3e5cfdbdf7
mirola777 <jmmunoza@eafit.edu.co>
2024-09-25 20:01:17 -0500

    Calculator app created with flask

---
+++ calculator.py
@@ -0,0 +1,8 @@
+class Calculator:
+    @staticmethod
+    def cuadrado(numero):
+        return numero * numero
+
+    @staticmethod
+    def cubo(numero):
+        return numero * numero * numero
---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, request
+
+from calculator import Calculator
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Bienvenido! Esta aplicación ayuda a elevar números a sus potencias."
+
+@app.route("/hola")
+def hola():
+    nombre = request.args.get("nombre", "Mundo")
+    return f"Hola {nombre}!"
+
+@app.route("/cuadrado")
+def cuadrado():
+    numero = float(request.args.get("numero", 0))
+    calculadora = Calculator()
+    return f"El cuadrado de {numero} es {calculadora.cuadrado(numero)}"
+
+@app.route("/cubo")
+def cubo():
+    numero = float(request.args.get("numero", 0))
+    calculadora = Calculator()
+    return f"El cubo de {numero} es {calculadora.cubo(numero)}"
+
+if __name__ == "__main__":
+    app.run(debug=True)
