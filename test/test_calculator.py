55ca9123f6332cafafe30d8bfa401e7a866177f9
mirola777 <jmmunoza@eafit.edu.co>
2024-09-25 20:48:20 -0500

    Integration and Unit tests added with unittest

---
+++ test/test_calculator.py
@@ -0,0 +1,47 @@
+import unittest
+
+from calculator import Calculator
+
+
+class TestCalculator(unittest.TestCase):
+
+    # Pruebas unitarias para el cuadrado
+    def test_cuadrado_positivo(self):
+        calc = Calculator()
+        self.assertEqual(calc.cuadrado(5.0), 25.0)
+
+    def test_cuadrado_cero(self):
+        calc = Calculator()
+        self.assertEqual(calc.cuadrado(0.0), 0.0)
+
+    def test_cuadrado_uno(self):
+        calc = Calculator()
+        self.assertEqual(calc.cuadrado(1.0), 1.0)
+
+    def test_cuadrado_negativo(self):
+        calc = Calculator()
+        self.assertEqual(calc.cuadrado(-4.0), 16.0)
+
+    # Pruebas unitarias para el cubo
+    def test_cubo_positivo(self):
+        calc = Calculator()
+        self.assertEqual(calc.cubo(5.0), 125.0)
+
+    def test_cubo_cero(self):
+        calc = Calculator()
+        self.assertEqual(calc.cubo(0.0), 0.0)
+
+    def test_cubo_uno(self):
+        calc = Calculator()
+        self.assertEqual(calc.cubo(1.0), 1.0)
+
+    def test_cubo_menos_uno(self):
+        calc = Calculator()
+        self.assertEqual(calc.cubo(-1.0), -1.0)
+
+    def test_cubo_negativo(self):
+        calc = Calculator()
+        self.assertEqual(calc.cubo(-4.0), -64.0)
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ test/test_http_request.py
@@ -0,0 +1,35 @@
+import unittest
+
+from main import app
+
+
+class TestHttpRequest(unittest.TestCase):
+
+    @classmethod
+    def setUp(self):
+        self.ctx = app.app_context()
+        self.ctx.push()
+        self.client = app.test_client()
+
+    def test_saludo_por_defecto(self):
+        response = self.client.get("/hola")
+        self.assertIn("Hola Mundo!", response.text)
+
+    def test_cuadrado_por_defecto(self):
+        response = self.client.get("/cuadrado")
+        self.assertRegex(response.text, r"El cuadrado de 0.0 es 0.0")
+
+    def test_cubo_por_defecto(self):
+        response = self.client.get("/cubo")
+        self.assertRegex(response.text, r"El cubo de 0.0 es 0.0")
+
+    def test_cuadrado_numero(self):
+        response = self.client.get("/cuadrado?numero=3")
+        self.assertRegex(response.text, r"El cuadrado de 3.0 es 9.0")
+
+    def test_cubo_numero(self):
+        response = self.client.get("/cubo?numero=3")
+        self.assertRegex(response.text, r"El cubo de 3.0 es 27.0")
+
+if __name__ == "__main__":
+    unittest.main()
