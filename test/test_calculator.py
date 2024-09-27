b6d10526a8fe9c0a1a0bdfd2b7640ed89ba50979
Juan Manuel <jmmunoza@eafit.edu.co>
2024-09-27 15:50:20 -0500

    Refactor Jenkinsfile to remove unused code and fix pylint path

--- test/test_calculator.py
+++ test/test_calculator.py
@@ -1,6 +1,6 @@
 import unittest
 
-from calculator import Calculator
+from src.calculator import Calculator
 
 
 class TestCalculator(unittest.TestCase):
--- test/test_http_request.py
+++ test/test_http_request.py
@@ -1,6 +1,6 @@
 import unittest
 
-from main import app
+from src.main import app
 
 
 class TestHttpRequest(unittest.TestCase):
